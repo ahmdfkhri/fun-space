4ea96685652a9681fdde243293d64222b3ef1fdc
ahmdfkhri <fakhri.ahmad030304@gmail.com>
2024-09-04 22:07:49 +0700

    Fungsi luas segitiga

---
+++ ahmad_fakhri.py
@@ -0,0 +1,6 @@
+# Hitung luas segitiga sederhana
+def luas_segitiga():
+  a = int(input("Masukkan alas segitiga: "))
+  t = int(input("Masukkan tinggi segitiga: "))
+  luas = a * t / 2
+  print("Luas segitiga adalah: ", luas)
